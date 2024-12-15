33fae725fdde7008fe1b93aed335cef424522883
mason <wybztn@hotmail.com>
2024-12-15 21:35:13 +0800

    init

---
+++ packages/common-ui/src/lib/common-ui.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledCommonUi = styled.div`
+  color: pink;
+`;
+
+export function CommonUi() {
+  return (
+    <StyledCommonUi>
+      <h1>Welcome to CommonUi!</h1>
+    </StyledCommonUi>
+  );
+}
+
+export default CommonUi;
